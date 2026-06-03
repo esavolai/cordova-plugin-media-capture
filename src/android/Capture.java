5d4b4a07e3c803f2bacd786e638eea95c4d5cc30
Erik Savolainen <esavolai@gmail.com>
2026-06-02 23:27:11 -0400

    Remove READ_MEDIA_* permissions
    Remove READ_MEDIA_* permissions

--- src/android/Capture.java
+++ src/android/Capture.java
@@ -287,8 +287,6 @@ public class Capture extends CordovaPlugin {
      * Sets up an intent to capture images.  Result handled by onActivityResult()
      */
     private void captureImage(Request req) {
-        if (isMissingCameraPermissions(req, Manifest.permission.READ_MEDIA_IMAGES)) return;
-
         // Save the number of images currently on disk for later
         this.numPics = queryImgDB(whichContentStore()).getCount();
 
